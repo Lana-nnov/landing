6b1568538aad7f3832025c6e87db5f2360626d51
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-08-07 01:20:24 +0300

    Начинаем верстку

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Личный проект «Пинк» [![Build status][travis-image]][travis-url] [![Dependency status][dependency-image]][dependency-url]
+
+* Студент:  [Светлана Ощепкова](https://up.htmlacademy.ru/adaptive/12/user/398413).
+* Наставник:  [].
+
+---
+
+**Обратите внимание на файл:**
+
+- [Contributing.md](Contributing.md) — руководство по внесению изменений.
+
+_Не удаляйте и не обращайте внимание на файлы:_<br>
+_`.editorconfig`, `.gitattributes`, `.gitignore`, `.stylelintrc`, `.travis.yml`, `package-lock.json`, `package.json`._
+
+---
+
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss"); //не установился? ругался npm
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var webp = require("gulp-webp");
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/js/**/*.js").on("change", server.reload);
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("start", gulp.series("css", "server"));
